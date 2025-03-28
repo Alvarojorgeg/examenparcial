ce94d7424c0eb11367649092ab40b58c06cf4477
Álvaro <145665238+Alvarojorgeg@users.noreply.github.com>
2025-03-28 08:29:24 +0000

    prueba 2 y añadir funciones

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# Hecho por Alvaro Jorge Gonzalez
+
 import math
+math.sqrt(9)
 
 class Punto:
     def __init__(self, x=0, y=0):
@@ -7,3 +10,21 @@ class Punto:
 
     def __str__(self):
         return f"({self.x}, {self.y})"
+
+#cuadrantes
+
+    def cuadrante(self):
+        if self.x > 0 and self.y > 0:
+            return "Primer cuadrante"
+        elif self.x < 0 and self.y > 0:
+            return "Segundo cuadrante"
+        elif self.x < 0 and self.y < 0:
+            return "Tercer cuadrante"
+        elif self.x > 0 and self.y < 0:
+            return "Cuarto cuadrante"
+        elif self.x == 0 and self.y != 0:
+            return "Eje Y"
+        elif self.x != 0 and self.y == 0:
+            return "Eje X"
+        else:
+            return "Origen"
