0b0fb2cb068239dc9f734c1b923dd0762f80df7d
Álvaro <145665238+Alvarojorgeg@users.noreply.github.com>
2025-03-28 08:34:34 +0000

    confirmar si se emparejan

--- main.py
+++ main.py
@@ -1,39 +1,5 @@
 # Hecho por Alvaro Jorge Gonzalez
 
-import math
-math.sqrt(9)
+from punto import Punto
+from rectangulo import Rectangulo
 
-#clase punto
-
-class Punto:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-#clase cuadrantes
-
-    def cuadrante(self):
-        if self.x > 0 and self.y > 0:
-            return "Primer cuadrante"
-        elif self.x < 0 and self.y > 0:
-            return "Segundo cuadrante"
-        elif self.x < 0 and self.y < 0:
-            return "Tercer cuadrante"
-        elif self.x > 0 and self.y < 0:
-            return "Cuarto cuadrante"
-        elif self.x == 0 and self.y != 0:
-            return "Eje Y"
-        elif self.x != 0 and self.y == 0:
-            return "Eje X"
-        else:
-            return "Origen"
-
-
-    def vector(self, otro):
-        return (otro.x - self.x, otro.y - self.y)
-
-    def distancia(self, otro):
-        return math.sqrt((otro.x - self.x)**2 + (otro.y - self.y)**2)
